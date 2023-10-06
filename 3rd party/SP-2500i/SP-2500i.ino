6f0b05f7cb3f1d408107a1fdaee19bc4330eae5c
Diego Torres Armenta <imt.dtorres@gmail.com>
2023-10-06 09:33:22 -0600

    Improvements in device simulator
    - Added Set and Get speed commands.
    - Changed order of commands as to avoid entering the wrong one.

--- 3rd party/SP-2500i/SP-2500i.ino
+++ 3rd party/SP-2500i/SP-2500i.ino
@@ -1,52 +1,72 @@
 String data; //variable para comandos miscelaneos
-String temp;
+String temp;  
 double current_pos = 0;
 double desired_pos = 0;
+unsigned int moving = 0;
+double current_speed = 100.00;
 
 void setup()
-{                     //Inicion del void setup.
-  Serial.begin(9600); //Inicia el puerto.
-  while(!Serial) { Serial.println("Conectando a puerto..."); } 
-}                     //Fin del void setup.
+{                     
+  Serial.begin(9600); //Open connection to port
+  while(!Serial) { 
+    
+  } 
+}                     
 
 void loop(){                    //Inicio del void loop.
   int index;
-  String text;
-
-  if (current_pos < desired_pos){
-    current_pos += 20;
-  }
-  else if (current_pos > desired_pos){
-    current_pos -= 20;
-  }
-  //Serial.println(current_pos);
+  String text;  
   
+  if (moving){
+    if (current_pos < desired_pos){
+      current_pos += 20;
+    }
+    else if (current_pos > desired_pos){
+      current_pos -= 20;
+    }
+    delay(50);
+  //Serial.println(current_pos);
+  }
+
+  if (current_pos == desired_pos){ moving = 0; }  
   
   if (Serial.available()){
     data = Serial.readStringUntil('\r');
     Serial.read();    // Discard any remaining characters after \r
     delay(100);
-
-    //index = data.indexOf("GOTO");
+    
     if (data.indexOf("GOTO") != -1){
       temp = data.substring(0, index-2);
       desired_pos = temp.toDouble();
       delay(1000);
       current_pos = desired_pos;
-      //Serial.print(" 1  ok\r\n");
     } 
     
     else if (data.indexOf("MODEL") != -1){
       Serial.print(" SP-2-500i  ok\r\n");    
     }
+
+    else if (data.indexOf("?NM/MIN")!=-1){
+      //Serial.println(current_speed);
+      Serial.print(" " + String(current_speed) + " nm/min ok\r\n");  
+    }       
+
+    else if (data.indexOf("NM/MIN") != -1){
+      temp = data.substring(0, index-2);
+      current_speed = temp.toDouble();
+      //Serial.println(current_speed);
+    }
     
     else if (data.indexOf(">NM")!=-1){
       temp = data.substring(0, index-2);
-      desired_pos = temp.toDouble();      
+      desired_pos = temp.toDouble();     
+      moving = 1; 
     }
+    
     else if (data.indexOf("?NM")!=-1){
       Serial.print(" " + String(current_pos) + " nm ok\r\n");  
     }
+    
     else if (data.indexOf("MONO-?DONE") != -1){
       if (current_pos == desired_pos){
         Serial.print(" 1  ok\r\n");         
@@ -54,8 +74,6 @@ void loop(){                    //Inicio del void loop.
       else{
         Serial.print(" 0  ok\r\n");
       }      
-    }
-  
-       
+    }    
   } //Fin del if de la interrupcion del puerto serial.  
 }
